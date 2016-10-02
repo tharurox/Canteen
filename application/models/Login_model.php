5211b02388e9d8e07d35f05f15cd53cc1ec756e9
Sanjula <it14119804@my.sliit.lk>
2016-10-02 12:41:30 +0530

    Add files via upload

---
+++ application/models/Login_model.php
@@ -0,0 +1,62 @@
+<?php
+class Login_model extends CI_Model
+{
+    public function _construct()
+    {
+		parent::_construct();
+	}
+	/*
+		*
+	*/
+	public function checkLogin($username,$password)
+	{
+		
+		$this->load->library('session');
+
+		try
+		{
+			$this->load->database();
+			$this->db->select("*");
+			$this->db->from("user");
+			$this->db->where("uname", $username);
+			
+			$query=$this->db->get();
+			$query->result();
+
+			foreach ($query->result() as $row){
+
+				if($row->uname==$username && $row->upassword==$password){
+					
+					$newdata = array(
+						'uname'  => $row->uname,
+						'uid'     => $row->uid,
+						'logged_in' => TRUE,
+						'ulevel' => $row->ulevel,
+						
+					);
+
+					$this->session->set_userdata($newdata);
+					
+					return True;
+				}
+				else
+				{
+					return false;
+				}
+				
+			}
+			return false;
+		}
+		catch(Exception $ex)
+		{
+			return FALSE;
+		}
+	}
+
+
+	
+
+	
+    
+}
+?>
